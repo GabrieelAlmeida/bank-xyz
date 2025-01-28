a7bbad5ce03507a0af581d26224b442f1ec43511
Gabriel Bezerra <gabriel.bezerra@global.ntt>
2025-01-28 16:48:49 -0300

    refactor: adjusting AuthLayout page

--- src/pages/Layouts/Auth/index.tsx
+++ src/pages/Layouts/Auth/index.tsx
@@ -1,11 +1,15 @@
 import { Outlet } from "react-router-dom";
 import { Logo } from "../../../components/Logo";
+import financialBackgroundLogo from "../../../assets/financial-login.svg";
 
 export function AuthLayout() {
     return(
         <main className="h-full w-full">
             <div className="flex justify-between text-brand-font-dark-100">
-                <Logo />
+                <div className="w-[50vw] h-screen bg-brand-dark-background-200  p-6 flex flex-col gap-10">
+                    <Logo />
+                    <img src={financialBackgroundLogo} className="justify-center "alt="Bank XYZ financial background Logo"/>
+                </div>
                 <Outlet />
             </div>
         </main>
