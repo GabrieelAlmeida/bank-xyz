7048f58def4e32cd4a6df3c2298b5dab1d565316
Gabriel Bezerra <gabriel.bezerra@global.ntt>
2025-01-29 04:21:01 -0300

    feat: add account informations utils functions

---
+++ src/utils/account.util.ts
@@ -0,0 +1,51 @@
+import { Transfer, TransferListResponse } from "../interfaces/account.interface";
+
+export function formatCurrency(balance: any, currency: any) {
+    return balance?.toLocaleString("en-US", {
+        style: 'currency',
+        currency: currency ?? 'USD'
+    });
+}
+
+export function formatTransfersToGraphList(transfersList: TransferListResponse) {
+    if (transfersList?.transfers) {
+        return transfersList?.transfers?.map((transfer) => ({
+            value: transfer?.value,
+            date: transfer?.date
+        }))
+            .sort((a, b) => new Date(a?.date).getTime() - new Date(b?.date).getTime());
+    }
+    return [];
+}
+
+export function getTotalTransfers(transfers: Transfer[]) {
+    if (transfers) {
+        return transfers?.length;
+    }
+
+    return 0;
+}
+
+export function getHighestTransfer(transfers: Transfer[]) {
+    if (transfers) {
+        const highestTransfer = transfers.reduce((max, transfer) => transfer?.value > max?.value ? transfer : max, transfers[0]);
+
+        return highestTransfer?.value;
+    }
+
+    return 0;
+}
+
+export function getAverageTransfers(transfers: Transfer[]) {
+    if (transfers) {
+        const totalTransfers = transfers?.length;
+        const sumTransfers = transfers.reduce((total, transfer) => total + Number(transfer?.value), 0);
+
+        const averageTransfers = Number(sumTransfers) / totalTransfers;
+
+        return averageTransfers;
+
+    }
+
+    return 0;
+}
