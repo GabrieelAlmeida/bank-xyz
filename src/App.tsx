68bcade0e963104a9d7bf3386b7d1d15f0874ed8
Gabriel Bezerra <gabriel.bezerra@global.ntt>
2025-01-30 01:23:43 -0300

    feat: add react query

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,21 @@
-import { RouterProvider } from 'react-router-dom'
-import { routes } from './routes.tsx'
-import { AuthProvider } from './context/AuthContext.tsx'
-import { CookiesProvider } from 'react-cookie'
+import { RouterProvider } from "react-router-dom";
+import { routes } from "./routes.tsx";
+import { AuthProvider } from "./context/AuthContext.tsx";
+import { CookiesProvider } from "react-cookie";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-      <CookiesProvider>
-        <AuthProvider>
+    <CookiesProvider>
+      <AuthProvider>
+        <QueryClientProvider client={queryClient}>
           <RouterProvider router={routes} />
-        </AuthProvider>
-      </CookiesProvider>
-  )
+        </QueryClientProvider>
+      </AuthProvider>
+    </CookiesProvider>
+  );
 }
 
-export default App
+export default App;
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,23 +1,42 @@
-import { createBrowserRouter, createRoutesFromElements, Route } from "react-router-dom";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+} from "react-router-dom";
 import { Login } from "./pages/Login";
 import { NotFound } from "./pages/NotFound";
-import { Dashboard } from "./pages/Dashboard";
+import Dashboard from "./pages/Dashboard";
 import { ProtectedRoute } from "./components/ProtectedRoute";
 import { AuthLayout } from "./pages/Layouts/Auth";
 import { DashboardLayout } from "./pages/Layouts/Dashboard";
+import { Transfers } from "./pages/Transfers";
 
-export const routes = createBrowserRouter(createRoutesFromElements(
+export const routes = createBrowserRouter(
+  createRoutesFromElements(
     <>
-        <Route path="/" element={<AuthLayout />} errorElement={<NotFound />}>
-            <Route path="login" index element={<Login />} />
-        </Route>
+      <Route path="/" element={<AuthLayout />} errorElement={<NotFound />}>
+        <Route path="login" index element={<Login />} />
+      </Route>
 
-        <Route path="/" element={<DashboardLayout />} errorElement={<NotFound />} >
-            <Route path="dashboard" element={
-                <ProtectedRoute>
-                    <Dashboard />
-                </ProtectedRoute>
-            } />
-        </Route>
-    </>
-));
+      <Route path="/" element={<DashboardLayout />} errorElement={<NotFound />}>
+        <Route
+          path="dashboard"
+          element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="transfers"
+          element={
+            <ProtectedRoute>
+              <Transfers />
+            </ProtectedRoute>
+          }
+        />
+      </Route>
+    </>,
+  ),
+);
